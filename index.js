a138bd9ec3e33702a2948186e4d7448e0a6aa021
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-09-25 19:51:12 +0530

    updating

--- index.js
+++ index.js
@@ -1,60 +1,21 @@
 var dust = require('dust')();
 var serand = require('serand');
 
-var profile = require('accounts-profile');
-var signup = require('accounts-signup');
+dust.loadSource(dust.compile(require('./template'), 'accounts-home'));
 
-var ready = false;
-
-var user;
-
-var context;
-
-serand.on('user', 'ready', function (usr) {
-    user = usr;
-    ready = true;
-    if (!context) {
-        return;
-    }
-    user ? profile(context.sandbox, user, context.done) : signup(context.sandbox, {}, context.done);
-});
-
-serand.on('user', 'logged in', function (usr) {
-    user = usr;
-    if (!context) {
-        return;
-    }
-    context.destroy();
-    profile(context.sandbox, function (err, destroy) {
-        context.destroy = err ? serand.none : destroy;
-    }, user);
-});
-
-serand.on('user', 'logged out', function () {
-    user = null;
-    if (!context) {
-        return;
-    }
-    context.destroy();
-    signup(context.sandbox, function (err, destroy) {
-        context.destroy = err ? serand.none : destroy;
-    }, user);
-});
-
-module.exports = function (sandbox, options, done) {
-    context = {
-        sandbox: sandbox,
-        options: options,
-        done: function (err, destroy) {
-            context.destroy = destroy;
-            done(err, function () {
-                destroy();
-                context = null;
-            });
+module.exports = function (ctx, sandbox, options, done) {
+    options = options || {};
+    dust.render('accounts-home', {
+        username: options.username
+    }, function (err, out) {
+        if (err) {
+            return done(err);
         }
-    };
-    if (!ready) {
-        return;
-    }
-    user ? profile(context.sandbox, user, context.done) : signup(context.sandbox, {}, context.done);
+        sandbox.append(out);
+        done(null, {
+            clean: function () {
+                $('.accounts-home', sandbox).remove();
+            }
+        });
+    });
 };
